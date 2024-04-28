719b342675851f7a3cd4155695b87dc4754a6f14
joeyanovich <joedison.dias@gmail.com>
2024-04-27 23:51:09 -0300

    feat ✨: header app layout

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -30,8 +30,6 @@ export function SignUp() {
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
-      console.log(data)
-
       toast.success('Restaurante cadastrado com sucesso', {
         action: {
           label: 'Login',
